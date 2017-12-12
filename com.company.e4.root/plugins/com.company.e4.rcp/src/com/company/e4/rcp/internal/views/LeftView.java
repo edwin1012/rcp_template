c7b5e96d0a67e7d8c6b946292bf2520fe0750d7c
edwin <kjhkjh900@suresofttech.com>
2017-12-13 08:57:24 +0900

    뷰 추가

---
+++ com.company.e4.root/plugins/com.company.e4.rcp/src/com/company/e4/rcp/internal/views/LeftView.java
@@ -0,0 +1,13 @@
+package com.company.e4.rcp.internal.views;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class LeftView {
+
+	@PostConstruct
+	public void createControl(Composite parent) {
+		
+	}
+}
