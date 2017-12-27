7cf53dc88660f403cc7fcc0d25a440b088637ba3
edwin <kjhkjh900@suresofttech.com>
2017-12-27 09:27:31 +0900

    변경

--- com.company.e4.root/plugins/com.company.e4.rcp/src/com/company/e4/rcp/internal/handlers/ThemeSwitchHandler.java
+++ com.company.e4.root/plugins/com.company.e4.rcp/src/com/company/e4/rcp/internal/handlers/ThemeSwitchHandler.java
@@ -1,21 +1,24 @@
 package com.company.e4.rcp.internal.handlers;
 
+import javax.inject.Inject;
+
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.css.swt.theme.IThemeEngine;
 
 public class ThemeSwitchHandler {
 	private static final String DEFAULT_THEME = "com.company.e4.rcp.theme.default";
 	private static final String NOTHING_THEME = "com.company.e4.rcp.theme.nothing";
 
-//	@Inject
-//	private IThemeEngine engine;
+	@Inject
+	private IThemeEngine engine;
 	
 	@Execute
 	public void switchTheme() {
-//		if (engine.getActiveTheme().getId().equals(DEFAULT_THEME)) {
-//			engine.setTheme(NOTHING_THEME, true);
-//		}
-//		else {
-//			engine.setTheme(DEFAULT_THEME, true);
-//		}
+		if (engine.getActiveTheme().getId().equals(DEFAULT_THEME)) {
+			engine.setTheme(NOTHING_THEME, true);
+		}
+		else {
+			engine.setTheme(DEFAULT_THEME, true);
+		}
 	}
 }
